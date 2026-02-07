6646e700ab80d8a77de91c40c6f6d9a2d9eacea8
dtishonok <dtishonok@gmail.com>
2026-02-07 16:30:12 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
-
 const app = require('./app');
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+server.on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${PORT} is already in use. Trying port ${PORT + 1}...`);
+    server.listen(PORT + 1);
+  } else {
+    console.error(err);
+  }
+});
