4af19380149bcb1a7d7da86f372eadc6b1a1b91d
dtishonok <dtishonok@gmail.com>
2026-02-07 14:20:46 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,6 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('OK');
+app.listen(PORT, () => {
+  console.log(`Server: http://localhost:${PORT}`);
 });
-
-module.exports = app; /
