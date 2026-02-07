baf3a2fde7a23cc7d4d0b6a47b1f32ce76a537eb
dtishonok <dtishonok@gmail.com>
2026-02-07 03:11:49 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,27 +1,19 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'postgres',
-  password: '123',
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-const setupDatabase = async () => {
-  try {
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100),
-        email VARCHAR(100) UNIQUE NOT NULL,
-        is_blocked BOOLEAN DEFAULT FALSE,
-        status VARCHAR(20) DEFAULT 'active',
-        last_login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
-    `);
-  } catch (err) { console.error(err.message); }
-};
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error(err.stack);
+  }
+  console.log('Connected to Postgres');
+  release();
+});
 
-setupDatabase();
 module.exports = pool;
