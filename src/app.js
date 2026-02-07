b0461e6386af519759e2129ed806e2ddb1c0c3e7
dtishonok <dtishonok@gmail.com>
2026-02-07 14:46:41 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -196,12 +196,13 @@ app.get('/logout', (req, res) => {
     res.redirect('/login');
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
+
 
 
 
